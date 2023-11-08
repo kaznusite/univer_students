d9a40d1a390dd3eba74fee664a90bfa639dcb394
Нурсапи Ержан <Yerzhan@kaznu.kz>
2023-11-08 15:11:08 +0600

    Initial commit

---
+++ dist/assets/js/pages/modal-add-role.js
@@ -0,0 +1,1 @@
+!function(){var e=$("#addRoleForm");e.length&&e.validate({rules:{modalRoleName:{required:!0}}}),$(".modal").on("hidden.bs.modal",function(){$(this).find("form")[0].reset()});const o=document.querySelector("#selectAll"),t=document.querySelectorAll('[type="checkbox"]');o.addEventListener("change",o=>{t.forEach(e=>{e.checked=o.target.checked})})}();
